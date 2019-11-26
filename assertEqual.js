47c8621a396148cad988d5b71f20e182ef79928e
Tausif <engr.tausifkhan@gmail.com>
2019-11-26 00:35:17 +0000

    created the assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  const equal = '😀 ' + ' Assertion Passed:';
+  const notEqual = '😞 ' + ' Assertion Failed:';
+
+  if (actual === expected) {
+    console.log(equal + ' ' + [actual] + ' === ' + [expected]);
+  } else {
+    console.log(notEqual + ' ' + [actual] + ' !== ' + [expected]);
+  }
+};
+
+assertEqual('Lighouse Labs', 'bootcamp');
+
+assertEqual(1, 1);
