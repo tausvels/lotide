aff118954cfb5aac810dfa5eaaef9aaa9289355d
Tausif <engr.tausifkhan@gmail.com>
2019-12-02 08:58:44 +0000

    add the eqArrays.js and the test file to the test folder

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,9 @@
-const assertEqual = function(actual, expected) {
-  const equal = '😀 ' + ' Assertion Passed:';
-  const notEqual = '😞 ' + ' Assertion Failed:';
-  
-  if (actual === expected) {
-    return (equal + ' ' + [actual] + ' === ' + [expected]);
-  } else {
-    return (notEqual + ' ' + [actual] + ' !== ' + [expected]);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const eqArray = function(actual,expected) {
   let equal = true;
-  if (actual.length !== expected.length) equal = false;
+  if (actual.length !== expected.length) 
+  equal = false;
   actual.forEach((item, ind)=>{
     if (item !== expected[ind]) {
       equal = false;
@@ -19,7 +11,5 @@ const eqArray = function(actual,expected) {
   });
   return equal;
 };
-console.log(assertEqual(eqArray([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArray([1, 2, 3], [3, 2, 1]), true));
-console.log(assertEqual(eqArray(["1", "2", "3"], ["1", "2", "3"]), true));
-console.log(assertEqual(eqArray(["1", "2", "3"], ["1", "2", 3]), true));
+
+module.exports = eqArray;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require("../assertEqual");
+const eqArray = require("../eqArrays");
+
+console.log(assertEqual(eqArray([1, 2, 3], [1, 2, 3]), true));
+console.log(assertEqual(eqArray([1, 2, 3], [3, 2, 1]), false));
+console.log(assertEqual(eqArray(["1", "2", "3"], ["1", "2", "3"]), true));
+console.log(assertEqual(eqArray(["1", "2", "3"], ["1", "2", 3]), false));
